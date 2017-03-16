5d6c9ecba7a43e741104a3b231123f06f8ecab55
MANISH-GIRI <manish.giri.me@gmail.com>
2017-03-15 22:27:42 -0400

    ArrayList of Parent type cannot accept list of child types

---
+++ src/corejava/genericswildcards/App.java
@@ -0,0 +1,55 @@
+package corejava.genericswildcards;
+
+import java.util.ArrayList;
+
+/**
+ * Created by manishgiri on 3/15/17.
+ */
+
+class Machine {
+    @Override
+    public String toString() {
+        return "I am a Machine";
+    }
+}
+
+class Camera extends Machine{
+
+    @Override
+    public String toString() {
+        return "I am a Camera";
+    }
+}
+public class App {
+    private static void showList(ArrayList<Machine> list) {
+        System.out.println("In showlist method");
+        for(Machine machine: list) {
+            System.out.println(machine);
+        }
+
+    }
+    public static void main(String[] args) {
+        ArrayList<Machine> machines = new ArrayList<>();
+        machines.add(new Machine());
+        machines.add(new Machine());
+
+        //machines.add(new Camera());
+
+        for(Machine machine: machines) {
+            //System.out.println(machine);
+        }
+
+        ArrayList<Camera> cameras = new ArrayList<>();
+        cameras.add(new Camera());
+        cameras.add(new Camera());
+
+        for(Camera camera: cameras) {
+            //System.out.println(camera);
+        }
+
+        showList(machines);
+        showList(cameras);
+
+
+    }
+}
